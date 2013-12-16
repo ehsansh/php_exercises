15a5e1b3368a76abd57db1cf31788f9b73a57029
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2013-12-16 23:02:58 +0330

    Create login_core.inc.php

---
+++ login_core.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+ob_start();
+session_start();
+$current_file = $_SERVER['SCRIPT_NAME'];
+//$http_referer = $_SERVER['HTTP_REFERER'];
+
+// function loggedin(){
+// 	if(  ){
+// 		return true;
+// 	} else{
+// 		return false;
+// 	}
+// }
+
+
+// function getfield($field){
+
+// 	$query = "SELECT `$field` FROM `users` WHERE `id` = '".$_SESSION['user_id']."'  ";
+// 	if( $query_run = mysql_query($query) ){
+// 		return $query_result = mysql_result($query_run, 0, $field);
+// 	}
+
+// }
+
+
+
+?>
