68f03c0618878733b5e9466d08cba083fa95a163
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2013-12-16 22:57:40 +0330

    Create hit_counter_count.php

---
+++ hit_counter_count.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+	<?php
+
+		function hit_count(){
+
+			$ip_address = $_SERVER['REMOTE_ADDR'];
+
+			$ip_file = file('ip.txt');
+			foreach ( $ip_file as $ip) {
+
+				$ip_single = trim($ip);
+				if( $ip_single == $ip_address ){
+					$found = true;
+					break;
+				}else{
+					$found = false;
+				}			
+			}
+			if( $found == false ){
+				$filename = 'count.txt';
+				$handle = fopen($filename, 'r');
+				$current = fread($handle, filesize($filename));
+				fclose($handle);
+
+				$current_inc = $current+1 ;
+
+				$handle = fopen($filename, 'w');
+				fwrite($handle, $current_inc);
+				fclose($handle);
+
+				$handle = fopen('ip.txt', 'a');
+				fwrite($handle, $ip_address."\n");
+				fclose($handle);
+			}
+		}
+
+
+	?>
+
+
+	
+</body>
+</html>
